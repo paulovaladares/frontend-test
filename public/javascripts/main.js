0a16e04d5ec9ac0f1d54e866a369b57b3a374be7
ViniciusJordao <vcs.jordao@gmail.com>
2015-03-26 11:07:05 -0700

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,5 @@
-// your code
+	var app = angular.module('myApp', []);
+	app.controller('fazenda', function($scope, $http) {
+  $http.get("fazenda.json")
+  .success(function (response) {$scope.names = response;});
+});
